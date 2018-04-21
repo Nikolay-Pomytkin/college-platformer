ac4ee8fc2d02042c5ac4a50bd9b54ab028d5901e
Nikolay Pomytkin <nikolayp2800@gmail.com>
2018-04-21 14:25:50 -0400

    i finally made something work

--- game.py
+++ game.py
@@ -10,6 +10,32 @@ SCREEN_HEIGHT = SPRITE_SIZE * 10
 
 MOVEMENT_SPEED = 5
 
+class Screen:
+    '''
+    This class holds information about the screens.
+    '''
+    def __init__(self):
+        self.wall_list = None
+        self.background = None
+
+def MainScreen():
+    ''' Create and return the screen for the main screen '''
+    main_screen = Screen()
+    main_screen.background = arcade.load_texture("images/main_background.png")
+    return main_screen
+
+def CreditsScreen():
+    ''' Create and return the screen for the credits screen '''
+    var = 1
+def setup_screen_1():
+    # Level 1: College Bully
+    var = 2
+def setup_screen_2():
+    # Level 2: Midterms
+    var = 3
+def setup_screen_3():
+    # Level 3: Finals
+    var = 4
 class PlatformerGame(arcade.Window):
     ''' Class that holds main application functions: initializer, update, draw, etc... '''
 
@@ -30,12 +56,16 @@ class PlatformerGame(arcade.Window):
 
         # set up the player
         self.life = 3
-        self.player_sprite  = arcade.Sprite("images/character.jpg", SPRITE_SCALING)
+        self.player_sprite  = arcade.Sprite("images/character.png", SPRITE_SCALING)
         self.player_sprite.center_x = 100
         self.player_sprite.center_y = 100
 
         # setup screens
         self.screens = []
+        screen = MainScreen()
+        self.screens.append(screen)
+        screen = CreditsScreen()
+        self.screens.append(screen)
         screen = setup_screen_1()
         self.screens.append(screen)
         screen = setup_screen_2()
@@ -50,37 +80,39 @@ class PlatformerGame(arcade.Window):
         ''' Actually render the screen '''
 
         arcade.start_render()
+        print(self.screens[self.current_screen])
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.screens[self.current_screen].background)
-        self.screens[self.current_screen].wall_list.draw()
+        if self.screens[self.current_screen].wall_list != None:
+            self.screens[self.current_screen].wall_list.draw()
 
         self.player_sprite.draw()
 
         def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
+            ''' Called whenever a key is pressed. '''
 
-        if key == arcade.key.UP:
-            self.player_sprite.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.player_sprite.change_y = -MOVEMENT_SPEED
-        elif key == arcade.key.LEFT:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = MOVEMENT_SPEED
+            if key == arcade.key.UP:
+                self.player_sprite.change_y = MOVEMENT_SPEED
+            elif key == arcade.key.DOWN:
+                self.player_sprite.change_y = -MOVEMENT_SPEED
+            elif key == arcade.key.LEFT:
+                self.player_sprite.change_x = -MOVEMENT_SPEED
+            elif key == arcade.key.RIGHT:
+                self.player_sprite.change_x = MOVEMENT_SPEED
 
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
+        def on_key_release(self, key, modifiers):
+            """Called when the user releases a key. """
 
-        if key == arcade.key.UP or key == arcade.key.DOWN:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.player_sprite.change_x = 0
+            if key == arcade.key.UP or key == arcade.key.DOWN:
+                self.player_sprite.change_y = 0
+            elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+                self.player_sprite.change_x = 0
 
     def update(self, delta_time):
         """ Movement and game logic """
 
         # Call update on all sprites (The sprites don't do much in this
         # example though.)
-        self.physics_engine.update()
+    #self.physics_engine.update()
 
         # Do some logic here to figure out what room we are in, and if we need to go
         # to a different room.
@@ -98,7 +130,7 @@ class PlatformerGame(arcade.Window):
 
 def main():
     """ Main method """
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = PlatformerGame(SCREEN_WIDTH, SCREEN_HEIGHT)
     window.setup()
     arcade.run()
 
